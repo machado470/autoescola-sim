8980d8ed92c2c7a0b060dcc9b81d672ade6b7d90
William Machado <williammachado5525@gmail.com>
2025-10-14 15:04:16 -0300

    fix(api): ajustes de schema, config e módulo principal

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
-import { HealthController } from './health/health.controller';
 import { PrismaModule } from './prisma/prisma.module';
+import { HealthController } from './health/health.controller';
+import { SchoolModule } from './school/school.module';
+import { InstructorModule } from './instructor/instructor.module';
 
 @Module({
-  imports: [PrismaModule],
+  imports: [
+    PrismaModule,
+    SchoolModule,
+    InstructorModule,
+  ],
   controllers: [HealthController],
   providers: [],
 })
