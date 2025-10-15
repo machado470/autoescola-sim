f57eb65b7ebfbfcb31027b84a828e89d566038e0
William Machado <williammachado5525@gmail.com>
2025-10-15 20:56:48 -0300

    fix(api): ajuste no main.ts para diagnóstico inicial da estrutura

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,10 +1,16 @@
+// apps/api/src/main.ts
 import 'reflect-metadata';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+// 👇 Corrige o erro "Do not know how to serialize a BigInt"
+(BigInt.prototype as any).toJSON = function () {
+  return this.toString();
+};
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, { cors: true });
   await app.listen(3000);
-  console.log('API running on http://localhost:3000');
+  console.log('🚀 API running on http://localhost:3000');
 }
 bootstrap();
